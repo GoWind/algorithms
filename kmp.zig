20a6f1835dfc02b38628106b7d7e229fafcdf54d
Govindarajan <govindnagarajan@outlook.com>
2022-04-29 14:00:05 +0200

    Add better documentation for KMP

--- kmp.zig
+++ kmp.zig
@@ -1,32 +1,38 @@
 const std = @import("std");
 
+// *proper* prefix: A prefix of an array a whose len < len(a)
+// *proper* suffix: A suffix of an array a whose len < len(a)
+
 // Return an array of border lengths where each element i
 // holds the length of the longest border of str[0..i]
 // A border is the longer *proper* suffix of str[0..i]
 // that is also a *proper* prefix of str[0..i]
+// An array of size 1 , cannot have a border, as it does not have a proper prefix or suffix
+// An array of size 2, can have a border, if and only if element 0 == element 1.
 fn borderArray(pattern: []const u8, allocator: std.mem.Allocator) ![]usize {
     var border_array = try allocator.alloc(usize, pattern.len);
     var i: usize = 1;
     var len: usize = 0; // holds the length of the current border under consideration
     border_array[0] = 0; // No valid border for a single character
     while (i < pattern.len) {
-        //pattern[0..len-1] is our current border
-        //we try to see if we can extend our border by one character
-        //to do this, we check if pattern[i] == pattern[len]
-        //if yes, we increment len and i
-        //if not, we find the border of the current border until len == 0
-        //or the next char after the boder == pattern[i]
+        // pattern[0..len-1] is our current border
+        // We try to see if we can extend our border by one character
+        // To do this, we check if pattern[i] == pattern[len]
+        // If yes, we increment len and i
+        // If not, we try to recursively find the border of the current border until len == 0
+        // or the next char after the border == pattern[i]
         if (pattern[len] == pattern[i]) {
-            len += 1; //current border is from 0..len-1 (=> length n). We are extending
-            //it by one
+            // current border is from pattern[0..len-1] (=> length n). We are extending
+            // it by one
+            len += 1;
             border_array[i] = len;
             i += 1;
         } else {
             if (len != 0) {
                 len = border_array[len - 1];
             } else {
-                border_array[i] = 0;
                 len = 0;
+                border_array[i] = len;
                 i += 1;
             }
         }
