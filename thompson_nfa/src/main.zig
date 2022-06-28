2f78600af4f17338ba9b740c1452b6938efec4d1
Govind <petrovgovind@gmail.com>
2022-06-28 16:57:21 +0200

    Finish Thomson NFA implementation !

--- thompson_nfa/src/main.zig
+++ thompson_nfa/src/main.zig
@@ -1,168 +1,292 @@
 const std = @import("std");
+const debug = std.debug;
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
 const heap = std.heap;
 const expect = std.testing.expect;
 const testing = std.testing;
 
-const State = struct { c: u8, out: ?*State, out1: ?*State, lastlist: i32 };
+const State = struct { label: ?u8 = null, out: ?*State = null, out1: ?*State = null };
+const NFA = struct { initial: *State, accept: *State };
+const NFAList = ArrayList(NFA);
+const StateSet = std.AutoHashMap(*State, bool);
 
-const StateEnum = enum { Match, Split };
-const ReErrors = error{
-    InvalidRe,
-};
+const MyError = error{MyErr};
 
-const Frag = struct { start: *State, out: *PtrList };
-const PtrType = enum { Next, State };
-const PtrList = ArrayList(State);
+fn get_prio(c: u8) u8 {
+    var val: u8 = switch (c) {
+        '*' => 60,
+        '+' => 55,
+        '?' => 50,
+        '.' => 40,
+        '|' => 20,
+        else => 0,
+    };
 
-const ParenState = struct { nalt: u32, natom: u32 };
-// We assume that buf has enough capacity to store postfix
-// representation of re
-fn re2post(re: []const u8, buf: []u8, parens_l: *ArrayList(ParenState)) !void {
-    var parens = parens_l.*;
-    var i: u32 = 0;
-    var bufi: u32 = 0;
-    var natom: u32 = 0;
-    var nalt: u32 = 0;
-    // var parenList = ArrayList(ParenState)
-    while (i < re.len) : (i += 1) {
-        switch (re[i]) {
+    return val;
+}
+
+test "get_prio test" {
+    try expect(get_prio('*') == 60);
+}
+const Err = error{ShuntErr};
+
+fn shunt(a: Allocator, infix: []const u8, postfix: []u8) !usize {
+    const shunt_state = struct { nalt: usize = 0, natom: usize = 0 };
+    var shunt_stack = ArrayList(shunt_state).init(a);
+    var ii: usize = 0; //index into infix
+    var pi: usize = 0; //index into postfix
+    var natom: usize = 0;
+    var nalt: usize = 0;
+    while (ii < infix.len) : (ii += 1) {
+        var char = infix[ii];
+        switch (char) {
             '(' => {
                 if (natom > 1) {
                     natom -= 1;
-                    buf[bufi] = '.';
-                    bufi += 1;
+                    postfix[pi] = '.';
+                    pi += 1;
                 }
-                const p = ParenState{ .nalt = nalt, .natom = natom };
-                try parens.append(p);
+                try shunt_stack.append(shunt_state{ .natom = natom, .nalt = nalt });
                 nalt = 0;
                 natom = 0;
             },
-            ')' => {
-                if (parens.items.len == 0) {
-                    return ReErrors.InvalidRe;
-                }
-                if (natom == 0) {
-                    return ReErrors.InvalidRe;
-                }
-                natom -= 1;
-                while (natom > 0) : (natom -= 1) {
-                    buf[bufi] = '.';
-                    bufi += 1;
-                }
-                while (nalt > 0) : (nalt -= 1) {
-                    buf[bufi] = '|';
-                    bufi += 1;
-                }
-                const p = parens.items[parens.items.len - 1];
-                // The corresponding `(` handler, sets nalt and natom to 0 when it see a new group. We are restoring the state prior to seeing the new group
-                _ = parens.pop();
-                nalt = p.nalt;
-                natom = p.natom;
-                natom += 1; // why ?
-            },
             '|' => {
                 if (natom == 0) {
-                    return ReErrors.InvalidRe;
+                    return Err.ShuntErr;
                 }
                 natom -= 1;
                 while (natom > 0) : (natom -= 1) {
-                    buf[bufi] = '.';
-                    bufi += 1;
+                    postfix[pi] = '.';
+                    pi += 1;
                 }
                 nalt += 1;
             },
+            ')' => {
+                if (shunt_stack.items.len == 0) {
+                    return Err.ShuntErr;
+                }
+                if (natom == 0) {
+                    return Err.ShuntErr;
+                }
+                natom -= 1;
+                while (natom > 0) : (natom -= 1) {
+                    postfix[pi] = '.';
+                    pi += 1;
+                }
+                while (nalt > 0) : (nalt -= 1) {
+                    postfix[pi] = '|';
+                    pi += 1;
+                }
+                var restore_stack = shunt_stack.pop();
+                nalt = restore_stack.nalt;
+                natom = restore_stack.natom;
+                natom += 1;
+            },
             '*', '+', '?' => {
                 if (natom == 0) {
-                    return ReErrors.InvalidRe;
+                    return Err.ShuntErr;
                 }
-                buf[bufi] = re[i];
-                bufi += 1;
+                //TODO: Test what happens when we have something
+                //like a++
+                postfix[pi] = char;
+                pi += 1;
             },
             else => {
                 if (natom > 1) {
                     natom -= 1;
-                    buf[bufi] = '.';
-                    bufi += 1;
+                    postfix[pi] = '.';
+                    pi += 1;
                 }
-                buf[bufi] = re[i];
+                postfix[pi] = char;
+                pi += 1;
                 natom += 1;
-                bufi += 1;
             },
         }
     }
-    if (parens.items.len != 0) {
-        return ReErrors.InvalidRe;
+    if (shunt_stack.items.len != 0) {
+        return Err.ShuntErr;
     }
-    while (natom > 0) : (natom -= 1) {
-        buf[bufi] = '.';
+    if (natom > 0) {
+        natom -= 1;
+        while (natom > 0) : (natom -= 1) {
+            postfix[pi] = '.';
+            pi += 1;
+        }
     }
-    while (nalt > 0) : (nalt -= 1) {
-        buf[bufi] = '|';
+    if (nalt > 0) {
+        nalt -= 1;
+        while (nalt > 0) : (nalt -= 1) {
+            postfix[pi] = '|';
+            pi += 1;
+        }
     }
+    shunt_stack.deinit();
+    return pi;
 }
 
-// fn make_state(a: Allocator, c: u8, out: ?*State, out1: ?*State) !*State {
-//     var s = try a.create(State);
-//     s.c = c;
-//     s.out = out;
-//     s.out1 = out1;
-//     return s;
-// }
-
-fn frag(start: *State, out: *PtrList) Frag {
-    var v = Frag{ .start = start, .out = out };
-    return v;
-}
-
-fn list1(s: State, a: Allocator) !PtrList {
-    var p = PtrList.init(a);
-    try p.append(s);
-    return p;
-}
-
-fn append(a: Allocator, p: *PtrList, q: *PtrList) !PtrList {
-    var r = PtrList.init(a);
-    var p_items = p.toOwnedSlice();
-    var q_items = q.toOwnedSlice();
-    try r.ensureTotalCapacity(q_items.len + p_items.len);
-    r.appendSliceAssumeCapacity(p_items);
-    r.appendSliceAssumeCapacity(q_items);
-    return r;
-}
-
-test "list1 test " {
+test "proper_shunt test " {
     var t = testing.allocator;
-    var s = State{ .c = 34, .out = null, .out1 = null, .lastlist = 400 };
-    var p = try list1(s, t);
-    defer p.deinit();
-    try expect(p.items[0].c == s.c);
+    var infix = "ab+cd".*;
+    var postfix = try t.alloc(u8, 2 * infix.len);
+    defer t.free(postfix);
+    _ = try shunt(t, &infix, postfix);
+    try expect(std.mem.eql(u8, postfix[0..8], "ab+.c.d."));
+    var infix2 = "ab+c*(e|d)?f".*;
+    var postfix2 = try t.alloc(u8, 2 * infix2.len);
+    defer t.free(postfix2);
+    _ = try shunt(t, &infix2, postfix2);
+    debug.print("postfix2 {s}\n", .{postfix2});
+    try expect(std.mem.eql(u8, postfix2[0..14], "ab+.c*.ed|?.f."));
 }
 
-test "append" {
+fn compile(a: Allocator, postfix: []const u8) !NFA {
+    var nfaStack = NFAList.init(a);
+    var i: usize = 0;
+    while (i < postfix.len) : (i += 1) {
+        const c = postfix[i];
+        switch (c) {
+            '*' => {
+                var nfa1 = nfaStack.pop();
+                var initial = try create_state(a);
+                var accept = try create_state(a);
+                initial.out = nfa1.initial;
+                initial.out1 = accept;
+                // point back to initial state to match more chars
+                nfa1.accept.out = nfa1.initial;
+                // point to the accept state on empty string
+                nfa1.accept.out1 = accept;
+                try nfaStack.append(NFA{ .initial = initial, .accept = accept });
+            },
+            '.' => {
+                var e2 = nfaStack.pop();
+                var e1 = nfaStack.pop();
+                e1.accept.out = e2.initial;
+                try nfaStack.append(NFA{ .initial = e1.initial, .accept = e2.accept });
+            },
+            '|' => {
+                var e2 = nfaStack.pop();
+                var e1 = nfaStack.pop();
+                var initial = try create_state(a);
+                initial.out = e1.initial;
+                initial.out1 = e2.initial;
+                var accept = try create_state(a);
+                e1.accept.out = accept;
+                e2.accept.out = accept;
+                try nfaStack.append(NFA{ .initial = initial, .accept = accept });
+            },
+            '+' => {
+                var e1 = nfaStack.pop();
+                var initial = try create_state(a);
+                var accept = try create_state(a);
+
+                initial.out = e1.initial;
+                e1.accept.out = e1.initial;
+                e1.accept.out1 = accept;
+                try nfaStack.append(NFA{ .initial = initial, .accept = accept });
+            },
+            '?' => {
+                var e1 = nfaStack.pop();
+                var initial = try create_state(a);
+                var accept = try create_state(a);
+                initial.out = e1.initial;
+                initial.out1 = accept;
+                e1.accept.out = accept;
+                try nfaStack.append(NFA{ .initial = initial, .accept = accept });
+            },
+            else => {
+                var initial = try create_state(a);
+                var accept = try create_state(a);
+                initial.label = c;
+                initial.out = accept;
+                try nfaStack.append(NFA{ .initial = initial, .accept = accept });
+            },
+        }
+    }
+    debug.assert(nfaStack.items.len == 1);
+    return nfaStack.pop();
+}
+
+fn create_state(a: Allocator) !*State {
+    var s = try a.create(State);
+    s.out = null;
+    s.out1 = null;
+    s.label = null;
+    return s;
+}
+
+fn follows(state: *State, state_set: *StateSet, level: u32) MyError!void {
+    _ = state_set.put(state, true) catch return MyError.MyErr;
+    if (state.label == null) {
+        if (state.out != null) {
+            try follows(state.out.?, state_set, level + 1);
+        }
+        if (state.out1 != null) {
+            try follows(state.out1.?, state_set, level + 1);
+        }
+    }
+    return;
+}
+
+test "follows test" {
+    var s = State{ .label = null, .out = null, .out1 = null };
+    var t = State{ .label = 'a', .out = null, .out1 = null };
+    var u = State{ .label = 'b', .out = null, .out1 = null };
+    s.out = &t;
+    s.out1 = &u;
+    var alloc = testing.allocator;
+    var hm = StateSet.init(alloc);
+    defer hm.deinit();
+    try follows(&s, &hm, 0);
+    try expect(hm.contains(&t) == true);
+    try expect(hm.contains(&u) == true);
+}
+
+test "compile fn test" {
+    var buffer: [4096]u8 = undefined;
+    var t = std.heap.FixedBufferAllocator.init(&buffer).allocator();
+    var nfa = compile(t, "ab.c.") catch @panic("failed to compile NFA");
+    try expect(nfa.initial.label.? == 'a');
+}
+
+fn match(a: Allocator, infix: []u8, input: []u8) !bool {
+    var currents = StateSet.init(a);
+    var nexts = StateSet.init(a);
+    var postfix = try a.alloc(u8, 2 * infix.len);
+    defer a.free(postfix);
+    var postfixlen = try shunt(a, infix, postfix);
+    postfix = a.resize(postfix, postfixlen) orelse return Err.ShuntErr;
+    debug.print("postfix is {s} and with resized len {}", .{ postfix, postfix.len });
+    var nfa = compile(a, postfix) catch @panic("failed to compile NFA");
+    // **ALGO STARTS HERE**
+    // Add initial state to current
+    try follows(nfa.initial, &currents, 0);
+    var ii: usize = 0;
+    while (ii < input.len) : (ii += 1) {
+        var c_iterator = currents.keyIterator();
+        while (c_iterator.next()) |key| {
+            if (key.*.label) |l| {
+                if (l == input[ii] and key.*.out != null) {
+                    follows(key.*.out.?, &nexts, 0) catch @panic("kaboom");
+                }
+            }
+        }
+        var temp_currents = currents;
+        currents = nexts;
+        nexts = StateSet.init(a);
+        temp_currents.deinit();
+    }
+    defer nexts.deinit();
+    return currents.contains(nfa.accept);
+}
+test "match test" {
     var t = testing.allocator;
-    var s = State{ .c = 34, .out = null, .out1 = null, .lastlist = 400 };
-    var paq = try list1(s, t);
-    std.debug.print("p len is {} and capacity is {}\n", .{ paq.items.len, paq.capacity });
-    try expect(paq.items[0].c == s.c);
-    var sa = State{ .c = 34, .out = null, .out1 = null, .lastlist = 400 };
-    var q = try list1(sa, t);
-    std.debug.print("q len is {} and capacity is {}\n", .{ q.items.len, q.capacity });
-    var r = try append(t, &paq, &q);
-    defer r.deinit();
-    try expect(r.items[0].c == s.c);
-    try expect(r.items[1].c == sa.c);
-    std.debug.print("\n\n\n Tests are done\n\n\n", .{});
+    var arena = std.heap.ArenaAllocator.init(t);
+    defer arena.deinit();
+    var h = arena.allocator();
+    var infix = "ab+".*;
+    var input = "abbb".*;
+    var matched = try match(h, &infix, &input);
+    try expect(matched == true);
 }
-
-pub fn main() anyerror!void {
-    var a = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    var arena = a.allocator();
-    const regex = "ab*cde(g|h)+";
-    const postFix = try arena.alignedAlloc(u8, null, 2 * regex.len);
-    //TODO: Begin tomorrow from here
-    var groupsList = ArrayList(ParenState).init(arena);
-    try re2post(regex, postFix, &groupsList);
-    std.debug.print("postFix regex is {s}\n", .{postFix});
-}
+pub fn main() anyerror!void {}
