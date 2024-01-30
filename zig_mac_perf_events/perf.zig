9e4a513fdbeb01bb67e91494dba0c82e17070731
Govind <petrovgovind@gmail.com>
2024-01-30 02:53:11 +0100

    Acquiring counters works without segfault-ing. Yay !

--- zig_mac_perf_events/perf.zig
+++ zig_mac_perf_events/perf.zig
@@ -34,12 +34,13 @@ const kpep_config = extern struct {
 const perfDb = struct {
     const Self = @This();
     dynlib: std.DynLib,
-    kpep_create_db: *const fn (?[*:0]const u8, **kpep_db) callconv(.C) i32,
+    kpep_db_create: *const fn (?[*:0]const u8, **kpep_db) callconv(.C) i32,
     kpep_db_events_count: *const fn (*kpep_db, *usize) callconv(.C) i32,
     kpep_db_events: *const fn (*kpep_db, [*]*kpep_event, usize) callconv(.C) i32,
+    kpep_db_event: *const fn (*kpep_db, [*:0]const u8, **kpep_event) callconv(.C) i32,
     kpep_config_create: *const fn (*kpep_db, **kpep_config) callconv(.C) i32,
     kpep_config_force_counters: *const fn (*kpep_config) callconv(.C) i32,
-    kpep_config_add_event: *const fn (*kpep_config, **kpep_event, u32, *u32) callconv(.C) i32,
+    kpep_config_add_event: *const fn (*kpep_config, **kpep_event, u32, ?*u32) callconv(.C) i32,
     // Get classes information
     kpep_config_kpc_classes: *const fn (*kpep_config, *u32) callconv(.C) i32,
     /// Get kpc register configs.
@@ -51,12 +52,14 @@ const perfDb = struct {
 
     pub fn new(d: std.DynLib) Self {
         var libPerfData = d;
-        return Self{ .dynlib = d, .kpep_create_db = libPerfData.lookup(*const fn (?[*:0]const u8, **kpep_db) callconv(.C) i32, "kpep_db_create").?, .kpep_db_events_count = libPerfData.lookup(*const fn (*kpep_db, *usize) callconv(.C) i32, "kpep_db_events_count").?, .kpep_db_events = libPerfData.lookup(*const fn (*kpep_db, [*]*kpep_event, usize) callconv(.C) i32, "kpep_db_events").?, .kpep_config_create = libPerfData.lookup(*const fn (*kpep_db, **kpep_config) callconv(.C) i32, "kpep_config_create").?, .kpep_config_force_counters = libPerfData.lookup(*const fn (*kpep_config) callconv(.C) i32, "kpep_config_force_counters").?, .kpep_config_add_event = libPerfData.lookup(*const fn (*kpep_config, **kpep_event, u32, *u32) callconv(.C) i32, "kpep_config_add_event").?, .kpep_config_kpc_classes = libPerfData.lookup(*const fn (*kpep_config, *u32) callconv(.C) i32, "kpep_config_kpc_classes").?, .kpep_config_kpc_count = libPerfData.lookup(*const fn (*kpep_config, *usize) callconv(.C) i32, "kpep_config_kpc_count").?, .kpep_config_kpc_map = libPerfData.lookup(*const fn (*kpep_config, *usize, usize) callconv(.C) i32, "kpep_config_kpc_map").?, .kpep_config_kpc = libPerfData.lookup(*const fn (*kpep_config, *kpc_config, usize) callconv(.C) i32, "kpep_config_kpc").? };
+        return Self{ .dynlib = d, .kpep_db_create = libPerfData.lookup(*const fn (?[*:0]const u8, **kpep_db) callconv(.C) i32, "kpep_db_create").?, .kpep_db_events_count = libPerfData.lookup(*const fn (*kpep_db, *usize) callconv(.C) i32, "kpep_db_events_count").?, .kpep_db_events = libPerfData.lookup(*const fn (*kpep_db, [*]*kpep_event, usize) callconv(.C) i32, "kpep_db_events").?, .kpep_config_create = libPerfData.lookup(*const fn (*kpep_db, **kpep_config) callconv(.C) i32, "kpep_config_create").?, .kpep_config_force_counters = libPerfData.lookup(*const fn (*kpep_config) callconv(.C) i32, "kpep_config_force_counters").?, .kpep_config_add_event = libPerfData.lookup(*const fn (*kpep_config, **kpep_event, u32, ?*u32) callconv(.C) i32, "kpep_config_add_event").?, .kpep_config_kpc_classes = libPerfData.lookup(*const fn (*kpep_config, *u32) callconv(.C) i32, "kpep_config_kpc_classes").?, .kpep_config_kpc_count = libPerfData.lookup(*const fn (*kpep_config, *usize) callconv(.C) i32, "kpep_config_kpc_count").?, .kpep_config_kpc_map = libPerfData.lookup(*const fn (*kpep_config, *usize, usize) callconv(.C) i32, "kpep_config_kpc_map").?, .kpep_config_kpc = libPerfData.lookup(*const fn (*kpep_config, *kpc_config, usize) callconv(.C) i32, "kpep_config_kpc").?, .kpep_db_event = libPerfData.lookup(*const fn (*kpep_db, [*:0]const u8, **kpep_event) callconv(.C) i32, "kpep_db_event").? };
     }
 };
-const perLib = struct {
+const perfLib = struct {
     const Self = @This();
 
+    kpc_pmu_version: *const fn () callconv(.C) u32,
+    kpc_force_all_ctrs_get: *const fn (*i32) callconv(.C) i32,
     /// acquire or release counters. param 1 = acquire, 0 = release
     kpc_force_all_ctrs_set: *const fn (i32) callconv(.C) i32,
 
@@ -70,34 +73,33 @@ const perLib = struct {
 
     pub fn new(d: std.DynLib) Self {
         var libPerf = d;
-        return Self{ .kpc_force_all_ctrs_set = libPerf.lookup(*const fn (i32) callconv(.C) i32, "kpc_force_all_ctrs_set").?, .kpc_set_config = libPerf.lookup(*const fn (u32, *kpc_config) callconv(.C) i32, "kpc_set_config").?, .kpc_set_counting = libPerf.lookup(*const fn (u32) callconv(.C) i32, "kpc_set_counting").?, .kpc_set_thread_counting = libPerf.lookup(*const fn (u32) callconv(.C) i32, "kpc_set_thread_counting").? };
+        return Self{ .kpc_pmu_version = libPerf.lookup(*const fn () callconv(.C) u32, "kpc_pmu_version").?, .kpc_force_all_ctrs_get = libPerf.lookup(*const fn (*i32) callconv(.C) i32, "kpc_force_all_ctrs_get").?, .kpc_force_all_ctrs_set = libPerf.lookup(*const fn (i32) callconv(.C) i32, "kpc_force_all_ctrs_set").?, .kpc_set_config = libPerf.lookup(*const fn (u32, *kpc_config) callconv(.C) i32, "kpc_set_config").?, .kpc_set_counting = libPerf.lookup(*const fn (u32) callconv(.C) i32, "kpc_set_counting").?, .kpc_set_thread_counting = libPerf.lookup(*const fn (u32) callconv(.C) i32, "kpc_set_thread_counting").? };
     }
 };
+/// The events we are interested in tracking;
+const m2MacEvents = [_][]const u8{ "FIXED_CYCLES", "FIXED_INSTRUCTIONS", "INST_BRANCH", "BRANCH_MISPRED_NONSPEC" };
+var counter_map = [_]usize{0} ** 32;
+/// The actual register values for each perf counter
+var regs = [_]u64{0} ** 32;
 pub fn main() !void {
     var libPerf = try std.DynLib.open("/System/Library/PrivateFrameworks/kperf.framework/kperf");
     var libPerfData = try std.DynLib.open("/System/Library/PrivateFrameworks/kperfdata.framework/kperfdata");
     defer libPerf.close();
     defer libPerfData.close();
+    const perf_lib = perfLib.new(libPerf);
+    const perf_doobi = perfDb.new(libPerfData);
 
     std.debug.print("found the dynamic lib we need\n", .{});
 
-    const prot_kpc_force_all_ctrs_get = *const fn (*i32) callconv(.C) i32;
-    const kpc_force_all_ctrs_get = libPerf.lookup(prot_kpc_force_all_ctrs_get, "kpc_force_all_ctrs_get").?;
-    const kpc_pmu_version = libPerf.lookup(*const fn () callconv(.C) u32, "kpc_pmu_version").?;
     var res: i32 = 0;
-    if (kpc_force_all_ctrs_get(&res) != 0) {
+    if (perf_lib.kpc_force_all_ctrs_get(&res) != 0) {
         std.debug.print("cannot get counter access, maybe you have to run as root", .{});
     } else {
         std.debug.print("success, can run as root\n", .{});
     }
-    std.debug.print("pmu version is {}\n", .{kpc_pmu_version()});
-
-    // const kpep_create_db = libPerfData.lookup(*const fn (?[*:0]const u8, **kpep_db) callconv(.C) i32, "kpep_db_create").?;
-    // const kpep_db_events_count = libPerfData.lookup(*const fn (*kpep_db, *usize) callconv(.C) i32, "kpep_db_events_count").?;
-    // const kpep_db_events = libPerfData.lookup(*const fn (*kpep_db, [*]*kpep_event, usize) callconv(.C) i32, "kpep_db_events").?;
-    const perf_doobi = perfDb.new(libPerfData);
+    std.debug.print("pmu version is {}\n", .{perf_lib.kpc_pmu_version()});
     var db: *kpep_db = undefined;
-    if (perf_doobi.kpep_create_db(null, &db) != 0) {
+    if (perf_doobi.kpep_db_create(null, &db) != 0) {
         std.debug.print("unable to read the perf database", .{});
     }
     std.debug.print("loaded db {s} with marketing name {s}\n", .{ db.*.name, db.*.marketing_name });
@@ -113,11 +115,63 @@ pub fn main() !void {
         std.debug.print("cant list out supported events \n", .{});
     }
 
-    var config: ?kpep_config = null;
+    var config: *kpep_config = undefined;
     if (perf_doobi.kpep_config_create(db, &config) != 0) {
         @panic("cannot create config for setting up perf counters");
     }
-    if (perf_doobi.kpep_config_force_counters(&config) != 0) {
+    if (perf_doobi.kpep_config_force_counters(config) != 0) {
         @panic("unable to force counters");
+    }
+
+    var found_events = [_]?*kpep_event{null} ** m2MacEvents.len;
+    for (m2MacEvents, 0..) |event, i| {
+        var maybe_event: *kpep_event = undefined;
+        if (perf_doobi.kpep_db_event(db, @as([*:0]const u8, @ptrCast(event)), &maybe_event) != 0) {
+            std.debug.panic("unable to obtain event {s} to track", .{event});
+        }
+        found_events[i] = maybe_event;
+        if (perf_doobi.kpep_config_add_event(config, &maybe_event, 0, null) != 0) {
+            std.debug.panic("failed to add event {s} to config\n", .{event});
+        }
+    }
+    var classes: u32 = 0;
+    var reg_count: usize = 0;
+    if (perf_doobi.kpep_config_kpc_classes(config, &classes) != 0) {
+        @panic("failed to get kpc classes");
+    }
+    std.debug.print("classes is {x}\n", .{classes});
+    if (perf_doobi.kpep_config_kpc_count(config, &reg_count) != 0) {
+        @panic("could get register count");
+    }
+    std.debug.print("register count is {}\n", .{reg_count});
+
+    if (perf_doobi.kpep_config_kpc_map(config, &counter_map[0], @sizeOf(usize) * counter_map.len) != 0) {
+        @panic("failed to get counter map");
+    }
+    // some sort of register config?
+    if (perf_doobi.kpep_config_kpc(config, &regs[0], @sizeOf(u64) * regs.len) != 0) {
+        @panic("Failed get kpc registers");
+    }
+
+    // Try to acquire registers needed for our perf counting from the Power Manager
+    if (perf_lib.kpc_force_all_ctrs_set(1) != 0 ) {
+        @panic("Failed force all ctrs");
+    }
+    const KPC_CLASS_CONFIGURABLE_MASK = 2;
+    // if we cant access the configurable counters throw error
+    if ((classes & KPC_CLASS_CONFIGURABLE_MASK != 0) and (reg_count > 0)) {
+        if (perf_lib.kpc_set_config(classes, &regs[0]) != 0) {
+            @panic("failed to set kpc config");
+        }
+    }
+
+    // enable counting for configurable counters
+    if (perf_lib.kpc_set_counting(classes) != 0) {
+        @panic("Failed set counting");
+    }
+
+    // Set PMC classes to enable counting for current thread.
+    if (perf_lib.kpc_set_thread_counting(classes) != 0) {
+        @panic("Failed set thread counting");
     }
 }
